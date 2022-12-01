1e701e489b5ce0b580a716024d38596d78cb930e
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2022-12-01 18:45:27 -0500

    Add files via upload

---
+++ routes/api/posts.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser")
+const User = require('../../schema/userSchema');
+const Post = require('../../schema/PostSchema');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// get reqest to get the post object
+router.get("/", async(req,res,next) => {
+        var searchObj = req.query;
+
+        if(searchObj.isReply !== undefined){
+            var isReply = searchObj.isReply == "true";
+            searchObj.replyTo = {$exists: isReply};
+            delete searchObj.isReply;
+        }
+
+        var results = await getPosts({searchObj});
+        res.status(200).send(results);
+})
+
+// get request to get the post ID
+router.get("/:id", async(req,res,next) => {
+    var postId = req.params.id;
+
+    var postData = await getPosts({ _id: postId});
+    postData = postData[0];
+
+    var results = {
+        postData: postData
+    }
+
+    if(postData.replyTo !== undefined){
+        results.replyTo = postData.replyTo;
+    }
+
+    results.replies = await getPosts({ replyTo: postId});
+    res.status(200).send(results);
+})
+
+router.post("/", async(req,res,next) => {
+
+    if(!req.body.content){
+        console.log("Content param not sent with request");
+        return res.sendStatus(400);
+    }
+
+    var postData = {
+        content: req.body.content,
+        postedBy: req.session.user
+    }
+
+    if(req.body.replyTo){
+        postData.replyTo = req.body.replyTo;
+    }
+
+    Post.create(postData)
+    .then(async newPost => {
+        newPost = await User.populate(newPost, { path: "postedBy"})
+    
+        res.status(201).send(newPost);
+    
+    })
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+})
+
+// output the likes to the post
+router.put("/:id/like", async (req,res,next)=>{
+    
+    var postId = req.params.id;
+    var userId = req.session.user._id;
+
+    var isLiked = req.session.user.likes && req.session.user.likes.includes(postId);
+
+    var option = isLiked ? "$pull" : "$addToSet";
+
+    // user like
+    req.session.user = await User.findByIdAndUpdate(userId, { [option]: { likes: postId }}, {new: true})
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+    // post like
+    var post = await Post.findByIdAndUpdate(postId, { [option]: { likes: userId }}, {new: true})
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+    res.status(200).send(post)
+})
+
+// output the retweets
+router.post("/:id/retweet", async (req,res,next)=>{
+    
+    
+    var postId = req.params.id;
+    var userId = req.session.user._id;
+
+    // Attempt to delete retweet
+
+    var deletedPost = await Post.findOneAndDelete({ postedBy: userId, retweetData: postId })
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+    var option = deletedPost != null ? "$pull" : "$addToSet";
+
+    var repost = deletedPost;
+
+    if(repost == null){
+        repost = await Post.create({ postedBy: userId, retweetData: postId })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        })
+    }
+
+    // user like
+    req.session.user = await User.findByIdAndUpdate(userId, { [option]: { retweets : repost._id }}, {new: true})
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+    // post like
+    var post = await Post.findByIdAndUpdate(postId, { [option]: { retweetUsers: userId }}, {new: true})
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+    res.status(200).send(post)
+}
+
+)
+
+// delete post by id
+router.delete("/:id", (req, res, next) =>{
+    Post.findByIdAndDelete(req.params.id)
+    .then(() => res.sendStatus(202))
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+})
+
+// get all posts and their data from database
+async function getPosts(filter){
+    var results = await Post.find(filter)
+    .populate("postedBy")
+    .populate("retweetData")
+    .populate("replyTo")
+    .sort({ "createdAt" :-1})
+    .catch(error => console.log(error))
+
+    results = await User.populate(results, {path:"replyTo.postedBy"});
+    return await User.populate(results, {path:"retweetData.postedBy"});
+
+    
+}
+
+module.exports = router;
